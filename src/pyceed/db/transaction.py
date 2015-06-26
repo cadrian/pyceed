aabe2576dfa18134bc5021037b7e8748563cf12d
Cyril Adrian <cyril.adrian@gmail.com>
2015-06-26 09:28:07 +0200

    transaction test

--- src/pyceed/db/transaction.py
+++ src/pyceed/db/transaction.py
@@ -1,23 +1,31 @@
 # Transaction: a central point of control for database objects with commit / rollback management
 
+from pyceed.db.internals import DbException, _DbObject
+
 class Transaction(object):
     def __init__(self, connection):
-        this.__cursor = connection.cursor()
-        this._map = {}
+        """
+        Create a new transaction bound to the given connection which must be compatible with apsw's
+        """
+        self.__connection = connection
+        self.__cursor = connection.cursor()
+        self._map = {}
 
     def select(self, factory, **kw):
         """
         Select an object of the given factory, create it if not found.
         Register and return that object.
         """
-        return factory(transaction=self, **kw)
+        with self.__connection:
+            return factory(transaction=self, **kw)
 
     def commit(self):
         """
         Commit all registered objects
         """
-        for obj in self.items():
-            obj.commit()
+        with self.__connection:
+            for obj in self.items():
+                obj.commit()
 
     def rollback(self):
         """
@@ -31,10 +39,11 @@ class Transaction(object):
         Iterate over all the registered objects
         """
         for instances in self._map.values():
-            for instance in instances:
+            for instance in instances.values():
                 yield instance
 
     def __getattr__(self, name):
         if name == "cursor":
             return self.__cursor
-        return super(Transaction, self).__getattr__(name)
+        else:
+            return super(Transaction, self).__getattr__(name)
