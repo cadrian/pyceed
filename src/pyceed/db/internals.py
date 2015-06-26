c28013bd9b51f54ffcad194c6090b60e3430ffb2
Cyril Adrian <cyril.adrian@gmail.com>
2015-06-26 07:43:10 +0200

    Moved object map to transaction. Less smelly code.

--- src/pyceed/db/internals.py
+++ src/pyceed/db/internals.py
@@ -1,116 +1,109 @@
 class DbException(Exception):
-	pass
+    pass
 
 class _DbObject(object):
-	"""
-	Parent of all database objects.
-	"""
+    """
+    Parent of all database objects.
+    """
 
-	__instances = dict()
+    def __new__(cls, transaction, rowid=None, **values):
+        """
+        Make sure that an instance with a given rowid is always the same instance
+        """
+        result = None
 
-	def __new__(cls, transaction, rowid=None, **values):
-		"""
-		Make sure that an instance with a given rowid is always the same instance
-		"""
-		result = None
+        columns = cls._columns
+        extra = dict()
+        for k,v in values.items():
+            if not k in columns:
+                extra[k] = v
+        for k in extra.keys():
+            del values[k]
 
-		columns = cls._columns
-		extra = dict()
-		for k,v in values.items():
-			if not k in columns:
-				extra[k] = v
-		for k in extra.keys():
-			del values[k]
+        instances = transaction._map.get(cls, None)
+        if instances is None:
+            instances = transaction._map[cls] = dict()
+            transaction.cursor.execute("create table if not exists %s (%s)" % (cls.__name__, ", ".join(columns)))
 
-		instances = _DbObject.__instances.get(cls, None)
-		if instances is None:
-			instances = _DbObject.__instances[cls] = dict()
-			transaction.cursor.execute("create table if not exists %s (%s)" % (cls.__name__, ", ".join(columns)))
+        if rowid is not None:
+            result = instances.get(rowid, None)
+        elif values:
+            for row in transaction.cursor.execute("select rowid from %s where %s" % (
+                cls.__name__,
+                " and ".join("%s = :%s" % (k,k) for k in values.keys()),
+            ), values):
+                if rowid is not None:
+                    raise DbException("More than one row for %s" % (
+                        ", ".join("%s = %s" % (k,v) for (k,v) in values.items()),
+                    ))
+                rowid = row[0]
+                result = instances.get(rowid, None)
 
-		if rowid is not None:
-			result = instances.get(rowid, None)
-		elif values:
-			for row in transaction.cursor.execute("select rowid from %s where %s" % (
-				cls.__name__,
-				" and ".join("%s = :%s" % (k,k) for k in values.keys()),
-			), values):
-				if rowid is not None:
-					raise DbException("More than one row for %s" % (
-						", ".join("%s = %s" % (k,v) for (k,v) in values.items()),
-					))
-				rowid = row[0]
-				result = instances.get(rowid, None)
+        if result:
+            result.update(**values)
+        else:
+            data = {k: None for k in columns}
 
-		if result:
-			result.update(**values)
-		else:
-			data = {k: None for k in columns}
+            if rowid is None:
+                data.update(values)
+                transaction.cursor.execute("insert into %s (%s) values (%s)" % (
+                    cls.__name__,
+                    ", ".join(columns),
+                    ", ".join(":%s" % (k,) for k in columns),
+                ), data)
+                rowid = transaction.cursor.getconnection().last_insert_rowid()
+            else:
+                for row in transaction.cursor.execute("select %s from %s where rowid = ?" % (
+                        ", ".join(columns),
+                        cls.__name__,
+                    ), (
+                        rowid,
+                    )):
+                    for i, col in enumerate(columns):
+                        data[col] = row[i]
 
-			if rowid is None:
-				data.update(values)
-				transaction.cursor.execute("insert into %s (%s) values (%s)" % (
-					cls.__name__,
-					", ".join(columns),
-					", ".join(":%s" % (k,) for k in columns),
-				), data)
-				rowid = transaction.cursor.getconnection().last_insert_rowid()
-			else:
-				for row in transaction.cursor.execute("select %s from %s where rowid = ?" % (
-						", ".join(columns),
-						cls.__name__,
-					), (
-						rowid,
-					)):
-					for i, col in enumerate(columns):
-						data[col] = row[i]
+            result = super(_DbObject, cls).__new__(cls)
+            result.__rowid = rowid
+            result.__data = data
+            result.__olddata = dict(data)
 
-			result = super(_DbObject, cls).__new__(cls)
-			result.__rowid = rowid
-			result.__data = data
-			result.__olddata = dict(data)
+            instances[rowid] = result
 
-			instances[rowid] = result
+        result.update(**extra)
+        return result
 
-		result.update(**extra)
-		return result
+    def __eq__(self, other):
+        return other is self
 
-	def __eq__(self, other):
-		return other is self
+    def update(self, **values):
+        for k,v in values.items():
+            setattr(self, k, v)
 
-	def update(self, **values):
-		for k,v in values.items():
-			setattr(self, k, v)
+    def commit(self, transaction):
+        if self.__olddata != self.__data:
+            transaction.cursor.execute("update %s set %s where rowid = :rowid" % (
+                self.__type__,
+                ", ".join(["%s = :%s" % (k,k) for k in self.__data.keys()]),
+            ), self.__data)
+            self.__olddata.update(self.__data)
 
-	def commit(self, transaction):
-		if self.__olddata != self.__data:
-			transaction.cursor.execute("update %s set %s where rowid = :rowid" % (
-				self.__type__,
-				", ".join(["%s = :%s" % (k,k) for k in self.__data.keys()]),
-			), self.__data)
-			self.__olddata.update(self.__data)
+    def rollback(self):
+        self.__data.update(self.__olddata)
 
-	def rollback(self):
-		self.__data.update(self.__olddata)
+    def __getattr__(self, name):
+        if name == "rowid":
+            return self.__rowid
+        elif name in self.__data:
+            return self.__data[name]
+        else:
+            raise AttributeError
 
-	def __getattr__(self, name):
-		if name == "rowid":
-			return self.__rowid
-		elif name in self.__data:
-			return self.__data[name]
-		else:
-			raise AttributeError
-
-	def __setattr__(self, name, value):
-		if name.startswith('_'):
-			super(_DbObject, self).__setattr__(name, value)
-		elif name == "rowid":
-			raise AttributeError("rowid cannot be changed")
-		elif name in self.__data:
-			self.__data[name] = value
-		else:
-			super(_DbObject, self).__setattr__(name, value)
-
-	@staticmethod
-	def _mop_up():
-		"for tests only!!"
-		_DbObject.__instances.clear()
+    def __setattr__(self, name, value):
+        if name.startswith('_'):
+            super(_DbObject, self).__setattr__(name, value)
+        elif name == "rowid":
+            raise AttributeError("rowid cannot be changed")
+        elif name in self.__data:
+            self.__data[name] = value
+        else:
+            super(_DbObject, self).__setattr__(name, value)
--- test/unit/db/test_internals.py
+++ test/unit/db/test_internals.py
@@ -3,67 +3,68 @@ from mockito import mock, when, verify, verifyNoMoreInteractions, inorder
 from pyceed.db.internals import _DbObject
 
 class FakeDbObject(_DbObject):
-	_columns = ("timestamp", "value")
+    _columns = ("timestamp", "value")
 
 class TestDbObject(unittest.TestCase):
-	def setUp(self):
-		_DbObject._mop_up()
-		self.transaction = mock()
-		self.cursor = mock()
-		self.connection = mock()
-		self.transaction.cursor = self.cursor
-		when(self.cursor).getconnection().thenReturn(self.connection)
+    def setUp(self):
+        self.transaction = mock()
+        self.transaction._map = {}
+        self.cursor = mock()
+        self.connection = mock()
+        self.transaction.cursor = self.cursor
+        when(self.cursor).getconnection().thenReturn(self.connection)
 
-	def test_create_empty(self):
-		when(self.connection).last_insert_rowid().thenReturn(42)
+    def test_create_empty(self):
+        when(self.connection).last_insert_rowid().thenReturn(42)
 
-		fdo = FakeDbObject(self.transaction)
+        fdo = FakeDbObject(self.transaction)
 
-		inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
-		inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
-			"timestamp": None,
-			"value": None,
-		})
-		inorder.verify(self.cursor, times=1).getconnection()
-		verifyNoMoreInteractions(self.cursor)
-		self.assertIsNone(fdo.value)
-		self.assertIsNone(fdo.timestamp)
-		self.assertTrue(fdo.rowid == 42)
+        inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
+        inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
+            "timestamp": None,
+            "value": None,
+        })
+        inorder.verify(self.cursor, times=1).getconnection()
+        verifyNoMoreInteractions(self.cursor)
+        self.assertIsNone(fdo.value)
+        self.assertIsNone(fdo.timestamp)
+        self.assertTrue(fdo.rowid == 42)
+        self.assertEqual({FakeDbObject: {42: fdo}}, self.transaction._map)
 
-	def test_create_same(self):
-		when(self.connection).last_insert_rowid().thenReturn(42)
+    def test_create_same(self):
+        when(self.connection).last_insert_rowid().thenReturn(42)
 
-		fdo = FakeDbObject(self.transaction)
+        fdo = FakeDbObject(self.transaction)
 
-		inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
-		inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
-			"timestamp": None,
-			"value": None,
-		})
-		inorder.verify(self.cursor, times=1).getconnection()
+        inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
+        inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
+            "timestamp": None,
+            "value": None,
+        })
+        inorder.verify(self.cursor, times=1).getconnection()
 
-		fdo2 = FakeDbObject(self.transaction, rowid=42)
-		verifyNoMoreInteractions(self.cursor)
+        fdo2 = FakeDbObject(self.transaction, rowid=42)
+        verifyNoMoreInteractions(self.cursor)
 
-		self.assertTrue(fdo is fdo2)
+        self.assertTrue(fdo is fdo2)
 
-	def test_create_dirty(self):
-		when(self.connection).last_insert_rowid().thenReturn(42)
+    def test_create_dirty(self):
+        when(self.connection).last_insert_rowid().thenReturn(42)
 
-		fdo = FakeDbObject(self.transaction)
+        fdo = FakeDbObject(self.transaction)
 
-		inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
-		inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
-			"timestamp": None,
-			"value": None,
-		})
-		inorder.verify(self.cursor, times=1).getconnection()
+        inorder.verify(self.cursor, times=1).execute("create table if not exists FakeDbObject (timestamp, value)")
+        inorder.verify(self.cursor, times=1).execute("insert into FakeDbObject (timestamp, value) values (:timestamp, :value)", {
+            "timestamp": None,
+            "value": None,
+        })
+        inorder.verify(self.cursor, times=1).getconnection()
 
-		fdo2 = FakeDbObject(self.transaction, rowid=42, value="foobar")
-		verifyNoMoreInteractions(self.cursor)
+        fdo2 = FakeDbObject(self.transaction, rowid=42, value="foobar")
+        verifyNoMoreInteractions(self.cursor)
 
-		self.assertEqual("foobar", fdo.value)
-		self.assertTrue(fdo is fdo2)
+        self.assertEqual("foobar", fdo.value)
+        self.assertTrue(fdo is fdo2)
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
