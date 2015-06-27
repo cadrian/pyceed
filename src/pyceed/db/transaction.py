336e50ac6b26bc032de726e765c2fbcd2271313e
Cyril Adrian <cyril.adrian@gmail.com>
2015-06-27 22:46:23 +0200

    better transaction api

--- src/pyceed/db/transaction.py
+++ src/pyceed/db/transaction.py
@@ -4,47 +4,67 @@ from pyceed.db.internals import DbException, _DbObject
 
 
 class Transaction(object):
-    def __init__(self, connection):
-        """
-        Create a new transaction bound to the given connection which must be compatible with apsw's
-        """
-        self.__connection = connection
-        self.__cursor = connection.cursor()
-        self._map = {}
+	def __init__(self, connection):
+		"""
+		Create a new transaction bound to the given connection which must be compatible with apsw's
+		"""
+		self.__connection = connection
+		self.__cursor = connection.cursor()
+		self._map = {}
 
-    def select(self, factory, **kw):
-        """
-        Select an object of the given factory, create it if not found.
-        Register and return that object.
-        """
-        with self.__connection:
-            return factory(transaction=self, **kw)
+	def select(self, factory, rowid, **kw):
+		"""
+		Select an object of the given factory, create it if not found.
+		Register and return that object.
+		"""
+		with self.__connection:
+			return factory(transaction=self, rowid=rowid, insert=None, **kw)
 
-    def commit(self):
-        """
-        Commit all registered objects
-        """
-        with self.__connection:
-            for obj in self.items():
-                obj.commit()
+	def select_all(self, factory, rowid=None, insert=None, **kw):
+		"""
+		Select a list of objects of the given factory, create them if not found.
+		Register and return those objects.
 
-    def rollback(self):
-        """
-        Rollback all the registered objects
-        """
-        for obj in self.items():
-            obj.rollback()
+		Variants:
+		* If rowid is specified, will return at most one object
+		* If insert=True, will always create a new object
+		* If insert=False, will never create a new object
+		"""
+		result = None
+		with self.__connection:
+			result = factory(transaction=self, rowid=rowid, insert=insert, **kw)
+		if rowid is not None:
+			if result:
+				result = [result]
+			else:
+				result = []
+		return result
 
-    def items(self):
-        """
-        Iterate over all the registered objects
-        """
-        for instances in self._map.values():
-            for instance in instances.values():
-                yield instance
+	def commit(self):
+		"""
+		Commit all registered objects
+		"""
+		with self.__connection:
+			for obj in self.items():
+				obj.commit()
 
-    def __getattr__(self, name):
-        if name == "cursor":
-            return self.__cursor
-        else:
-            return super(Transaction, self).__getattr__(name)
+	def rollback(self):
+		"""
+		Rollback all the registered objects
+		"""
+		for obj in self.items():
+			obj.rollback()
+
+	def items(self):
+		"""
+		Iterate over all the registered objects
+		"""
+		for instances in self._map.values():
+			for instance in instances.values():
+				yield instance
+
+	def __getattr__(self, name):
+		if name == "cursor":
+			return self.__cursor
+		else:
+			return super(Transaction, self).__getattr__(name)
--- test/unit/db/test_transaction.py
+++ test/unit/db/test_transaction.py
@@ -3,77 +3,76 @@ from mockito import mock, when, verify, verifyNoMoreInteractions, inorder
 from pyceed.db.transaction import Transaction
 
 class CallableMock(object):
-    def __init__(self, m=None):
-        self.mock = m or mock()
+	def __init__(self, m=None):
+		self.mock = m or mock()
 
-    def __call__(self, *a, **kw):
-        return self.mock.__call__(*a, **kw)
+	def __call__(self, *a, **kw):
+		return self.mock.__call__(*a, **kw)
 
-    def __enter__(self):
-        return self.mock.__enter__()
+	def __enter__(self):
+		return self.mock.__enter__()
 
-    def __exit__(self, *a, **kw):
-        return self.mock.__exit__(*a, **kw)
+	def __exit__(self, *a, **kw):
+		return self.mock.__exit__(*a, **kw)
 
-    def __getattr__(self, method_name):
-        return self.mock.__getattr__(method_name)
+	def __getattr__(self, method_name):
+		return self.mock.__getattr__(method_name)
 
 class TestTransaction(unittest.TestCase):
-    """
-    Test transactional stuff
-    """
+	"""
+	Test transactional stuff
+	"""
 
-    def setUp(self):
-        self.factory = CallableMock()
-        self.connection = CallableMock()
-        self.cursor = mock()
-        when(self.connection.mock).cursor().thenReturn(self.cursor)
+	def setUp(self):
+		self.factory = CallableMock()
+		self.connection = CallableMock()
+		self.cursor = mock()
+		when(self.connection.mock).cursor().thenReturn(self.cursor)
 
-    def test_select(self):
-        x = object()
-        transaction = Transaction(self.connection)
-        when(self.factory.mock).__call__(transaction=transaction, x="foo").thenReturn(x)
+	def test_select(self):
+		x = object()
+		transaction = Transaction(self.connection)
+		when(self.factory.mock).__call__(transaction=transaction, rowid=None, insert=None, x="foo").thenReturn([x])
 
-        fdo = transaction.select(self.factory, x="foo")
+		fdo = transaction.select_all(self.factory, x="foo")[0]
 
-        self.assertTrue(fdo is x)
+		self.assertTrue(fdo is x)
 
-        inorder.verify(self.connection.mock).cursor()
-        inorder.verify(self.connection.mock).__enter__()
-        inorder.verify(self.factory.mock).__call__(transaction=transaction, x="foo")
-        inorder.verify(self.connection.mock).__exit__(None, None, None)
-        verifyNoMoreInteractions(self.factory.mock, self.connection.mock)
+		inorder.verify(self.connection.mock).cursor()
+		inorder.verify(self.connection.mock).__enter__()
+		inorder.verify(self.connection.mock).__exit__(None, None, None)
+		verifyNoMoreInteractions(self.connection.mock)
 
-    def test_commit(self):
-        fdo = mock()
-        fdo.rowid = 42
-        fdo.x = "foo"
+	def test_commit(self):
+		fdo = mock()
+		fdo.rowid = 42
+		fdo.x = "foo"
 
-        transaction = Transaction(self.connection)
-        instances = {42: fdo}
-        transaction._map[object] = instances
-        when(self.factory.mock).__call__(transaction=transaction, x="foo").thenReturn(fdo)
+		transaction = Transaction(self.connection)
+		instances = {42: fdo}
+		transaction._map[object] = instances
+		when(self.factory.mock).__call__(transaction=transaction, x="foo").thenReturn(fdo)
 
-        transaction.commit()
+		transaction.commit()
 
-        inorder.verify(self.connection.mock).cursor()
-        inorder.verify(self.connection.mock).__enter__()
-        inorder.verify(fdo).commit()
-        inorder.verify(self.connection.mock).__exit__(None, None, None)
-        verifyNoMoreInteractions(fdo, self.factory.mock, self.connection.mock)
+		inorder.verify(self.connection.mock).cursor()
+		inorder.verify(self.connection.mock).__enter__()
+		inorder.verify(fdo).commit()
+		inorder.verify(self.connection.mock).__exit__(None, None, None)
+		verifyNoMoreInteractions(fdo, self.factory.mock, self.connection.mock)
 
-    def test_rollback(self):
-        fdo = mock()
-        fdo.rowid = 42
-        fdo.x = "foo"
+	def test_rollback(self):
+		fdo = mock()
+		fdo.rowid = 42
+		fdo.x = "foo"
 
-        transaction = Transaction(self.connection)
-        instances = {42: fdo}
-        transaction._map[object] = instances
-        when(self.factory.mock).__call__(transaction=transaction, x="foo").thenReturn(fdo)
+		transaction = Transaction(self.connection)
+		instances = {42: fdo}
+		transaction._map[object] = instances
+		when(self.factory.mock).__call__(transaction=transaction, x="foo").thenReturn(fdo)
 
-        transaction.rollback()
+		transaction.rollback()
 
-        inorder.verify(self.connection.mock).cursor()
-        inorder.verify(fdo).rollback()
-        verifyNoMoreInteractions(fdo, self.factory.mock, self.connection.mock)
+		inorder.verify(self.connection.mock).cursor()
+		inorder.verify(fdo).rollback()
+		verifyNoMoreInteractions(fdo, self.factory.mock, self.connection.mock)
