1479b546f80938f2d4805c6761bc826bfc41476a
Cyril Adrian <cyril.adrian@gmail.com>
2015-06-30 19:53:19 +0200

    more deps

--- install-deps.sh
+++ install-deps.sh
@@ -3,7 +3,7 @@
 cd $(dirname $(readlink -f $0))
 
 sudo aptitude update -q=2
-sudo aptitude install -q=2 libxml2-dev libxslt1-dev python3 python3-pip python3-apsw python3-bottle python3-mockito
+sudo aptitude install -q=1 libxml2-dev libxslt1-dev zlib1g-dev python3 python3-pip python3-apsw python3-bottle python3-mockito
 
 sudo pip3 install feedgen --upgrade
 
